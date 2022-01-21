ac72a2499f7bdbc73774278103dd3b7b8eff3aea
DiguyaDeveloper <diegocecconcwb@outlook.com>
2022-01-21 14:58:36 -0300

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, OnInit, VERSION } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { CeccoffButton } from './modules/shared/components/ceccoff-buttons/ceccoff-buttons.interface';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  buttons: BehaviorSubject<CeccoffButton[]>;
   name = 'Angular ' + VERSION.major;
+
+  ngOnInit(): void {
+    this.buttons.next([
+      {
+        label: 'CONTINUAR',
+        ariaLabel: 'Botao continuar',
+        color: 'primary',
+        type: 'mat-raised-button',
+      },
+    ]);
+  }
 }
