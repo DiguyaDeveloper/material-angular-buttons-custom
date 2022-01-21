faf150b1987334c10a6e68108cd4750347b44170
DiguyaDeveloper <diegocecconcwb@outlook.com>
2022-01-21 14:01:46 -0300

    Initial commit

---
+++ src/app/modules/material/material.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+@NgModule({
+  declarations: [],
+  exports: [CommonModule, MatDialogModule, MatButtonModule, MatIconModule],
+})
+export class MaterialModule {}
