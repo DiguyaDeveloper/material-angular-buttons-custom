570c64c084ec4a029bce6f13aea5f504664575e0
DiguyaDeveloper <diegocecconcwb@outlook.com>
2022-01-21 15:02:50 -0300

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,10 @@ import { CeccoffButton } from './modules/shared/components/ceccoff-buttons/cecco
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  buttons: BehaviorSubject<CeccoffButton[]>;
+  buttons: BehaviorSubject<CeccoffButton[]> = new BehaviorSubject<
+    CeccoffButton[]
+  >([]);
+
   name = 'Angular ' + VERSION.major;
 
   ngOnInit(): void {
@@ -18,6 +21,36 @@ export class AppComponent implements OnInit {
         ariaLabel: 'Botao continuar',
         color: 'primary',
         type: 'mat-raised-button',
+      },
+      {
+        label: 'CONTINUAR',
+        ariaLabel: 'Botao continuar',
+        color: 'primary',
+        type: 'mat-raised-button',
+      },
+      {
+        label: 'CONTINUAR',
+        ariaLabel: 'Botao continuar',
+        color: 'primary',
+        type: 'mat-raised-button',
+      },
+      {
+        label: 'CANCELAR',
+        ariaLabel: 'Botao cancelar',
+        color: 'primary',
+        type: 'mat-flat-button',
+      },
+      {
+        label: 'DESISTIR',
+        ariaLabel: 'Botao desistir',
+        color: 'primary',
+        type: 'mat-stroked-button',
+      },
+      {
+        label: 'DESISTIR',
+        ariaLabel: 'Botao desistir',
+        color: '',
+        type: 'mat-stroked-button',
       },
     ]);
   }
