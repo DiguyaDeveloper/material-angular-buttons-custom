92283221eaa330105dff32ebcc3ec64ee279b15a
DiguyaDeveloper <diegocecconcwb@outlook.com>
2022-02-09 11:35:41 -0300

    3 files modified, 4 added, 2 deleted

--- src/app/modules/shared/components/ceccoff-buttons/ceccoff-buttons.component.ts
+++ src/app/modules/shared/components/ceccoff-buttons/ceccoff-buttons.component.ts
@@ -5,7 +5,7 @@ import { CeccoffButton } from './ceccoff-buttons.interface';
 @Component({
   selector: 'ceccoff-buttons',
   templateUrl: './ceccoff-buttons.component.html',
-  styleUrls: ['./ceccoff-buttons.component.css'],
+  styleUrls: ['./ceccoff-buttons.component.scss'],
 })
 export class CeccoffButtonsComponent {
   @Input() buttons: BehaviorSubject<CeccoffButton[]> = new BehaviorSubject<
